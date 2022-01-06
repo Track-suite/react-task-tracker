5e91ad3e2a2a4a477c08d2e908e94ae615cbfe69
Asram Ahamed <asrama1998@gmail.com>
2022-01-06 14:16:23 +0530

    craeted add task and remider checkbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from './components/AddTask';
 
 function App() {
 
@@ -24,12 +25,23 @@ function App() {
       reminder: false,
     },
   ])
-  // const name = 'Brad'
-
+  
+  // Delete Task
+   const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !==
+    id))
+   }
+  //  Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id 
+    ? {...task, reminder: !task.reminder} : task))
+  }
   return (
     <div className="container">
      <Header  />
-     <Tasks tasks={tasks} />
+     <AddTask />
+     {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete=
+     {deleteTask}  onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   );
 }
