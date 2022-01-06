d781d0b725e53e0c3716c5fee7f4eab5a05617c1
Asram Ahamed <asrama1998@gmail.com>
2022-01-06 14:57:22 +0530

    created add task function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AddTask from './components/AddTask';
 
 function App() {
 
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -26,6 +28,16 @@ function App() {
     },
   ])
   
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 
+    10000) + 1 
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+
+  }
+
   // Delete Task
    const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !==
@@ -38,9 +50,12 @@ function App() {
   }
   return (
     <div className="container">
-     <Header  />
-     <AddTask />
-     {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete=
+     <Header onAdd={() => setShowAddTask
+    (!showAddTask)} 
+    showAdd={showAddTask}
+    />
+     {showAddTask && <AddTask onAdd={addTask}/>}
+    {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete=
      {deleteTask}  onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,29 @@
-import PropTypes from 'prop-types'
-import Buttons from './Buttons'
-const Header = ({title}) => {
-
-    const onClick = () => {
-        console.log('Click')
-    }
-
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Buttons color='green' text='Add' onClick=
-            {onClick}/>
-                 </header>
-    )
-}
+import PropTypes from "prop-types";
+import Buttons from "./Buttons";
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Buttons 
+      color={showAdd ? 'red' : 'green'} 
+      text={showAdd ? 'Close' : 'Add'} 
+      onClick={onAdd} />
+    </header>
+  );
+};
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-
-// CSS in Js 
+// CSS in Js
 // const headingStyle = {
 
-//     color: 'red', 
+//     color: 'red',
 //     backgroundColor: 'black'
 // }
 
-export default Header
+export default Header;
